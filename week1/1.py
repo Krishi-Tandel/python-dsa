55b1262a69fc977e433efe663698e9969cc610b2
Krishi <24amtics100@gmail.com>
2026-04-13 14:16:48 +0530

    Solved one python problem

---
+++ week1/1.py
@@ -0,0 +1,9 @@
+n = int(input())
+if n % 2 != 0:
+    print("Weird")
+elif n % 2 == 0 and range(2,6):
+    print("Not weird")
+elif n % 2 == 0 and range(6,21):
+    print("Weird")
+elif n % 2 == 0 and n>20:
+    print("not weird")          
